80c6960bfcb320a79883e158fcdf5b93767714b9
Pot Martin PLZ UUEFT <martin.pot@zf.com>
2023-09-29 08:49:59 +0200

    Screen Changing C# - v1.0.0

---
+++ App.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using PexelsDotNetSDK.Api;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Windows.Threading;
+using System.Runtime.CompilerServices;
+
+namespace ScreenChanging
+{
+
+
+
+    public partial class App : Application
+    {
+        private MyFunctions lambda = new MyFunctions();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+
+            var DirPath = "C:/Users/Z0161359/source/repos/ScreenChanging/datas/wallpapers/";
+
+            var AllFiles = Directory.EnumerateFiles(DirPath);
+
+            //if (AllFiles.Any())
+            //{
+            //    lambda.LastImagesPaths = AllFiles.ToList();
+            //    lambda.SetImagesOnGUI(win, DirPath);
+            //}
+
+            lambda.PexelsAPI(win);
+
+        }
+    }
+;
+
+
+
+
+    public partial class MyFunctions
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public List<string> LastImagesPaths = new List<string> { "", "", "", ""};
+
+        public string DirPath = "C:/Users/Z0161359/source/repos/ScreenChanging/datas/wallpapers/";
+
+
+        public async void PexelsAPI(MainWindow mainWindow)
+        {
+            string queryWord = mainWindow.query.Text;
+            //string queryWord ="desert";
+
+            //pr.Progress = 0.2;
+            //prog.Value = 20;
+
+            var pexelsClient = new PexelsClient("563492ad6f917000010000014f768f9c9134428b89da41e3726dd566");
+
+            var photosInformations = await pexelsClient.SearchPhotosAsync(queryWord, orientation:"landscape", size:"high", page:1, pageSize:4);
+
+            IEnumerable<PexelsDotNetSDK.Models.Photo> photosInformationsList = photosInformations.photos.Take(4);
+
+            var counter = 0;
+            foreach (var photo in photosInformationsList)
+            {
+                var date = DateTime.Now.ToString("_MMddyyyy_HHmmss_");
+                SaveImage(photo.source.original, Path.Combine(this.DirPath, photo.id + date + queryWord + ".jpeg"));
+                LastImagesPaths[counter] = Path.Combine(this.DirPath, photo.id + date + queryWord + ".jpeg");
+                counter++;
+
+            }
+
+            SetImagesOnGUI(mainWindow);
+            DeleteUnusedImages();
+
+        }
+
+
+        public void SaveImage(string URL, string PathToSave)
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(URL, PathToSave);
+        }
+
+
+        public void SetImagesOnGUI(MainWindow window)
+        {
+            //List<string> ImagesPaths = new List<string>{"1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"};
+            List<System.Windows.Shapes.Rectangle> wpp = new List<System.Windows.Shapes.Rectangle> { window.WPP1, window.WPP2, window.WPP3, window.WPP4 };
+
+            for(int i = 0; i < wpp.Count; i++)
+            {
+                if (File.Exists(LastImagesPaths[i]))
+                {
+                    wpp[i].Fill = new ImageBrush(new BitmapImage(new Uri(LastImagesPaths[i], UriKind.RelativeOrAbsolute)));
+                }
+                else
+                {
+                    wpp[i].Fill = new ImageBrush(new BitmapImage(new Uri("C:/Users/Z0161359/source/repos/ScreenChanging/datas/shutterstock_253521943.jpg", UriKind.RelativeOrAbsolute)));
+                }
+            }
+        }
+
+
+        public void DeleteUnusedImages()
+        {
+            var AllFiles = Directory.EnumerateFiles(this.DirPath);
+            
+            foreach (var file in AllFiles)
+            {
+                if (!LastImagesPaths.Contains(file))
+                {
+                    Trace.Write(file);
+                    try
+                    {
+                        File.Delete(file);
+                    }     
+                    catch { }
+                }
+            }
+        }
+
+        public void SetWallpaper(bool IsChecked, string tempPath)
+        {
+
+            tempPath = CheckFilterSetting(IsChecked, tempPath);
+
+            const int SPI_SETDESKWALLPAPER = 20;
+            const int SPIF_UPDATEINIFILE = 0x01;
+            const int SPIF_SENDWININICHANGE = 0x02;
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        public string CheckFilterSetting(bool IsChecked, string jpegFilePath)
+        {
+            string pngFilePath = "C:/Users/Z0161359/source/repos/ScreenChanging/datas/zf_autocruise_35.png";
+
+            if (IsChecked)
+            {
+                using (Image backgroundImage = Image.FromFile(jpegFilePath))
+                using (Image overlayImage = Image.FromFile(pngFilePath))
+                {
+                    // Création d'une nouvelle image de sortie avec les dimensions de l'image de fond
+                    using (Bitmap mergedImage = new Bitmap(overlayImage.Width, overlayImage.Height))
+                    {
+                        // Création d'un objet Graphics à partir de l'image de sortie
+                        using (Graphics graphics = Graphics.FromImage(mergedImage))
+                        {
+                            // Dessiner l'image de fond sur l'image de sortie
+                            graphics.DrawImage(backgroundImage, 0, 0, overlayImage.Width, overlayImage.Height);
+
+                            // Dessiner l'image avec transparence sur l'image de sortie
+                            graphics.DrawImage(overlayImage, 0, 0, overlayImage.Width, overlayImage.Height);
+                        }
+
+                        // Enregistrer l'image de sortie fusionnée au format JPEG
+                        string jpegFilePathWithFilter = "C:/Users/Z0161359/source/repos/ScreenChanging/datas/wallpapers/choosen_with_filter.jpeg";
+                        mergedImage.Save(jpegFilePathWithFilter, ImageFormat.Jpeg);
+
+                        return jpegFilePathWithFilter;
+                    }
+                }
+            }
+
+            return jpegFilePath;
+        }
+    }
+
+    public class ProgressDestination
+    {
+        public double Progress { get; set; } // in range 0,1
+        public string Description { get; set; } // Optional text 
+    }
+
+    public class ProgressPoller : INotifyPropertyChanged
+    {
+        public ProgressDestination Progress { get; } = new ProgressDestination();
+        private readonly DispatcherTimer timer;
+        public ProgressPoller()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += OnTick;
+            timer.Interval = TimeSpan.FromSeconds(0.2);
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Value = (int)(Progress.Progress * MaxValue);
+            Description = Progress.Description;
+            OnPropertyChanged(nameof(Value));
+            OnPropertyChanged(nameof(Description));
+        }
+
+        public int Value { get; set; }
+        public string Description { get; set; }
+        public int MaxValue { get; } = 100;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenChanging
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string ChoosenPath = "";
+        public MyFunctions MF = new MyFunctions();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.StrokeThickness = 2;
+
+            ScaleTransform scaleTransform = new ScaleTransform(1.0, 1.0);
+            Rectangle rectangle = (Rectangle)sender;
+            rectangle.RenderTransform = scaleTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                To = 1.1,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, animation);
+
+        }
+
+        private void Rectangle_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.StrokeThickness = 0;
+
+            ScaleTransform scaleTransform = new ScaleTransform(1.1, 1.1);
+            Rectangle rectangle = (Rectangle)sender;
+            rectangle.RenderTransform = scaleTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                To = 1.0,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, animation);
+        }
+
+        private void WPP_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+
+            ImageBrush imageBrush = rect.Fill as ImageBrush;
+            ImageSource imageSource = imageBrush.ImageSource;
+
+            if (imageSource is BitmapImage bitmapImage)
+            {
+                this.ChoosenPath = bitmapImage.UriSource.AbsolutePath;
+            }
+
+            WPP5.Fill = rect.Fill;
+            ShowModalContent();
+        }
+
+        private void ShowModalContent()
+        {
+            ModalContainer.Visibility = Visibility.Visible;
+        }
+
+        private void HideModalContent()
+        {
+            WPP5.Fill = null;
+            ModalContainer.Visibility = Visibility.Collapsed;
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+
+            RotateTransform rotateTransform = new RotateTransform(0){ CenterX = image.ActualHeight / 2, CenterY = image.ActualWidth / 2 }; ;
+      
+            image.RenderTransform = rotateTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                To = 90,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image image = (Image)sender;
+
+            RotateTransform rotateTransform = new RotateTransform(90) { CenterX = image.ActualHeight / 2, CenterY = image.ActualWidth / 2 }; ;
+
+            image.RenderTransform = rotateTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                To = 0,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            HideModalContent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MF.SetWallpaper((bool)AutocruiseBox.IsChecked, this.ChoosenPath);
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MF.PexelsAPI(this);
+        }
+
+
+        //private void query_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode == Keys.Enter)
+        //    {
+        //        // Enter key was pressed
+        //        Console.WriteLine("Enter key was pressed");
+        //        // Perform any actions you want to do when Enter is pressed
+        //    }
+        //}
+    }
+}
